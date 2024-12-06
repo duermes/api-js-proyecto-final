e75b69d8fae27a99cef0467cf93e90ba20f12d60
duermes <iloveposhitos@gmail.com>
2024-12-06 10:35:20 -0500

    added cors access control origin

--- src/app.js
+++ src/app.js
@@ -11,17 +11,12 @@ dotenv.config();
 export const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.options("*", cors());
-
 app.use(
   cors({
     origin: [
       "http://localhost:3000",
       "http://js-proyecto-final.vercel.app",
-      "https://js-proyecto-final.vercel.app",
-      "http://www.duermes.me",
       "https://www.duermes.me",
-      "https://duermes.me",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -30,27 +25,8 @@ app.use(
       "Authorization",
       "Access-Control-Allow-Origin",
     ],
-    exposedHeaders: ["Access-Control-Allow-Origin"],
   })
 );
-
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (
-    (origin && origin.includes("duermes.me")) ||
-    origin?.includes("js-proyecto-final.vercel.app") ||
-    origin === "http://localhost:3000"
-  ) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 
 app.use(express.urlencoded({ extended: true }));
 
