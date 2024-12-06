7a48311cad679f8ea32d077ec3088ee4a57e6913
duermes <iloveposhitos@gmail.com>
2024-12-06 10:40:30 -0500

    cambiado cors gracias a wilbur

--- src/app.js
+++ src/app.js
@@ -11,22 +11,7 @@ dotenv.config();
 export const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "http://js-proyecto-final.vercel.app",
-      "https://www.duermes.me",
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Access-Control-Allow-Origin",
-    ],
-  })
-);
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 
