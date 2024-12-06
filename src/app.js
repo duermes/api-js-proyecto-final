c93308551306e7bb1ba7720d77c859ecdacf547e
duermes <iloveposhitos@gmail.com>
2024-12-06 09:37:14 -0500

    asd

--- src/app.js
+++ src/app.js
@@ -13,7 +13,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://js-proyecto-final.vercel.app/"],
+    origin: [
+      "http://localhost:3000",
+      "http://js-proyecto-final.vercel.app/, https://www.duermes.me",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
