a0d03ec5ad597436fefc69194b5295f71d276afd
duermes <iloveposhitos@gmail.com>
2024-12-13 10:37:47 -0500

    probando nuevo middleware

--- src/app.js
+++ src/app.js
@@ -10,6 +10,30 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const app = express();
+const customCors = (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://js-proyecto-final.vercel.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  next();
+};
+
+app.use(customCors);
+
 app.use(
   helmet({
     crossOriginResourcePolicy: false,
